cf392f11103e05d055b1e03a4c600892100bc8be
leobragatti <leonardo.bragatti@gmail.com>
2012-05-31 17:15:26 -0300

    Listagem de música por album

--- catalogo/catalogo/music/urls.py
+++ catalogo/catalogo/music/urls.py
@@ -2,11 +2,12 @@
 
 from django.conf.urls import patterns, include, url
 
-from .views import listar_albuns, index
+from .views import listar_albuns, listar_songs, index
 
 urlpatterns = patterns('',
 
     url(r'^albuns/', listar_albuns, name='listar_albuns'),
+    url(r'^songs/(\d+)', listar_songs, name='listar_songs'),
 	url(r'', index, name='pagina_inicial'),
 
 )
--- catalogo/catalogo/music/views.py
+++ catalogo/catalogo/music/views.py
@@ -16,3 +16,8 @@ def listar_albuns(request):
 	albuns = Album.objects.all()
 	vars_template = {'albuns': albuns}
 	return render(request, 'music/albuns.html', vars_template)
+
+def listar_songs(request, album_id):
+	songs = Song.objects.filter(album_id = album_id).order_by('number')
+	vars_template = {'songs': songs}
+	return render(request, 'music/songs.html', vars_template)	
